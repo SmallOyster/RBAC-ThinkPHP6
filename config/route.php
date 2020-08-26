5c507ee8306d2a621737485f785964f0f58ca47b
Oyster Cheung <master@xshgzs.com>
2020-08-26 12:13:46 +0800

    build 00114: fix bug&delete useless code

--- config/route.php
+++ config/route.php
@@ -13,7 +13,7 @@ return [
     // 是否开启路由延迟解析
     'url_lazy_route'        => false,
     // 是否强制使用路由
-    'url_route_must'        => false,
+    'url_route_must'        => true,
     // 合并路由规则
     'route_rule_merge'      => false,
     // 路由是否完全匹配
@@ -21,7 +21,7 @@ return [
     // 访问控制器层名称
     'controller_layer'      => 'controller',
     // 空控制器名
-    'empty_controller'      => 'Error',
+    'empty_controller'      => ' ',
     // 是否使用控制器后缀
     'controller_suffix'     => false,
     // 默认的路由变量规则
